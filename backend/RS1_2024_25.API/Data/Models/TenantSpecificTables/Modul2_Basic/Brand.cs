2728afe65e60c0d9e535872eaa09bf1809fdfd88
AnesBojic <anes.bojic100@gmail.com>
2025-05-21 16:21:42 +0200

    Created model for Brands
    Created model for brands, enabled DB context relation, migrated it and updated database

---
+++ backend/RS1_2024_25.API/Data/Models/TenantSpecificTables/Modul2_Basic/Brand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using RS1_2024_25.API.Helper;
+using RS1_2024_25.API.Helper.BaseClasses;
+
+namespace RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul2_Basic
+{
+  
+    public class Brand : TenantSpecificTable
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ backend/RS1_2024_25.API/Migrations/20250521141243_brands.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RS1_2024_25.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class brands : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TenantId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Brands_Tenants_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "Tenants",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Brands_TenantId",
+                table: "Brands",
+                column: "TenantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Brands");
+        }
+    }
+}
