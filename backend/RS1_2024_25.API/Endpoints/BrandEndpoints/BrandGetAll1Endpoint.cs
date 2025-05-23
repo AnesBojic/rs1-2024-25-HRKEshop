1fd2dac78cfef017ef7ce450899c5be8d77207c0
AnesBojic <anes.bojic100@gmail.com>
2025-05-23 16:25:23 +0200

    Created endpoint for getting all brands

---
+++ backend/RS1_2024_25.API/Endpoints/BrandEndpoints/BrandGetAll1Endpoint.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Helper.Api;
+using static RS1_2024_25.API.Endpoints.BrandEndpoints.BrandGetAll1Endpoint;
+
+namespace RS1_2024_25.API.Endpoints.BrandEndpoints
+{
+    
+    [Route("brands")]
+    public class BrandGetAll1Endpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+     .WithoutRequest
+     .WithResult<BrandGetAll1Response[]>
+    {
+        [HttpGet("all")]
+        public override async Task<BrandGetAll1Response[]> HandleAsync(CancellationToken cancellationToken = default)
+        {
+            var result = await db.Brands
+                            .Select(b => new BrandGetAll1Response
+                            {
+                                ID = b.ID,
+                                Name = b.Name,
+                                TenantId = b.TenantId
+                                
+
+                            })
+                            .ToArrayAsync(cancellationToken);
+
+            return result;
+        }
+
+        public class BrandGetAll1Response
+        {
+            public required int ID { get; set; }
+            public required string Name { get; set; }
+            public required int TenantId { get; set; }
+
+        }
+    }
+}
