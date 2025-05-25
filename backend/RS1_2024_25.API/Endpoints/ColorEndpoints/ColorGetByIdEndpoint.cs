18e1e0656cabeaec16c3c5e5b69968604ef8a9e6
AnesBojic <anes.bojic100@gmail.com>
2025-05-25 21:13:19 +0200

    Created ColorGetById endpoint

---
+++ backend/RS1_2024_25.API/Endpoints/ColorEndpoints/ColorGetByIdEndpoint.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Helper.Api;
+using static RS1_2024_25.API.Endpoints.ColorEndpoints.ColorGetByIdEndpoint;
+
+namespace RS1_2024_25.API.Endpoints.ColorEndpoints
+{
+
+    [Route("color")]
+    public class ColorGetByIdEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+        .WithRequest<int>
+        .WithActionResult<ColorGetByIdResponse>
+    {
+        [HttpGet("{id}")]
+        public override async Task<ActionResult<ColorGetByIdResponse>> HandleAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var color = await db.Colors
+                                .Where(c => c.ID == id)
+                                .Select(c => new ColorGetByIdResponse
+                                {
+                                    ID = c.ID,
+                                    Name = c.Name,
+                                    Hex_Code = c.Hex_Code,
+
+                                })
+                                .FirstOrDefaultAsync(x => x.ID == id, cancellationToken);
+
+            if (color == null)
+            {
+                throw new ArgumentException("Color not found");
+            }
+
+
+            return Ok(color);
+        }
+
+        public class ColorGetByIdResponse
+        {
+            public required int ID { get; set; }
+            public required string Name { get; set; }
+            public required string Hex_Code { get; set; }
+
+        }
+    }
+}
