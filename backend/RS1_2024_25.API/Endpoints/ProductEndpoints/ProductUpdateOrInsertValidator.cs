bb4e9ae16aa88801546c527b089d82b4c2e31fc8
AnesBojic <anes.bojic100@gmail.com>
2025-05-29 02:27:47 +0200

    Added validator for Products

---
+++ backend/RS1_2024_25.API/Endpoints/ProductEndpoints/ProductUpdateOrInsertValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Endpoints.StudentEndpoints;
+
+namespace RS1_2024_25.API.Endpoints.ProductEndpoints;
+public class ProductUpdateOrInsertValidator : AbstractValidator<ProductUpdateOrInsertEndpoint.ProductUpdateOrInsertRequest>
+{
+    public ProductUpdateOrInsertValidator(ApplicationDbContext dbContext)
+    {
+
+        // Validacija StudentNumber
+        RuleFor(x => x.Price)
+            .GreaterThanOrEqualTo(1);
+
+
+        // Validacija Gender
+        RuleFor(x => x.Gender)
+            .IsInEnum().WithMessage("Pol mora biti validan (Male, Female, Other).");
+
+
+   
+    }
+}
