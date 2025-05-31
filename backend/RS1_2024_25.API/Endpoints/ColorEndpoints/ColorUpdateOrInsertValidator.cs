90349d24ebcb7fd317fad877f47a28e83febd55f
AnesBojic <anes.bojic100@gmail.com>
2025-05-31 21:36:08 +0200

    Added validator for colors

---
+++ backend/RS1_2024_25.API/Endpoints/ColorEndpoints/ColorUpdateOrInsertValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Endpoints.ProductEndpoints;
+
+namespace RS1_2024_25.API.Endpoints.ColorEndpoints;
+public class ColorUpdateOrInsertValidator : AbstractValidator<ColorUpdateOrInsertEndpoint.ColorUpdateOrInsertRequest>
+{
+    public ColorUpdateOrInsertValidator(ApplicationDbContext dbContext)
+    {
+
+        // Validation for hex code
+        RuleFor(x => x.Hex_Code)
+            .Length(6).WithMessage("Hex Code has to have 6 characters");
+
+
+        // Validation for name
+        RuleFor(x => x.Name)
+            .MinimumLength(3);
+
+
+
+    }
+}
