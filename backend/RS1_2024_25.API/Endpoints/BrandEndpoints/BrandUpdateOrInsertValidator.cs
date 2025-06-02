bb1bd31abbd7a5422cc53f94627667d5ed4ab021
AnesBojic <anes.bojic100@gmail.com>
2025-06-02 18:50:18 +0200

    Added validator for brand

---
+++ backend/RS1_2024_25.API/Endpoints/BrandEndpoints/BrandUpdateOrInsertValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Endpoints.ColorEndpoints;
+
+namespace RS1_2024_25.API.Endpoints.BrandEndpoints;
+public class BrandUpdateOrInsertValidator : AbstractValidator<BrandUpdateOrInsertEndpoint.BrandUpdateOrInsertRequest>
+{
+    public BrandUpdateOrInsertValidator(ApplicationDbContext dbContext)
+    {
+
+        // Validation for hex code
+        RuleFor(x => x.TenantId)
+            .NotNull().WithMessage("TenantId is required");
+
+
+
+        // Validation for name
+        RuleFor(x => x.Name)
+            .MinimumLength(3).WithMessage("Brand name has to have 3 or more characters");
+
+
+
+    }
+}
